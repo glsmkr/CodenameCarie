0062588d64a32ea6d178f1f6003a466b488ab898
glsmkr <julian.a.fordyce@gmail.com>
2019-06-30 22:51:06 -0400

    Delete default view controller and class and create a custom one

---
+++ Carie/WelcomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WelcomeViewController.swift
+//  Carie
+//
+//  Created by Julian A. Fordyce on 6/30/19.
+//  Copyright © 2019 Glas Labs. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
